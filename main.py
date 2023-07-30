15d67d0a508ee3e74d4d1e3b39e6adc86e7aead4
yogiyiorgos <georgefil@outlook.com>
2023-07-30 22:29:32 +0200

    PythonAPI01Jul30

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get('/')
 def root():
     return {'mesage': 'Welcome!'}
 
+
 @app.get('/posts')
 def get_posts():
     return {'data': 'This is the posts'}
+
+
+@app.post('/createposts')
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {'data': 'post'}
+
+# 1:22:46 CRUD operations diagram
